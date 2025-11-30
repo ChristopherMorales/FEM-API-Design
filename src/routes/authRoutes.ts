f0665556ecb2c764345740815ebe12fb0600adb4
ChristopherMorales <christopher.morales4752@gmail.com>
2025-11-30 18:29:43 -0400

    Added jwt

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,13 +1,27 @@
 import { Router } from 'express'
+import { register, login } from '../controllers/authController.ts'
+import { validateBody } from '../middleware/validation.ts'
+import { insertUserSchema } from '../db/schema.ts'
+import { z } from 'zod'
 
 const router = Router()
 
-router.post('/register', (req, res) => {
-  res.status(201).json({ message: 'user signed up' })
+// Login validation schema
+const loginSchema = z.object({
+  email: z.email('Invalid email format'),
+  password: z.string().min(1, 'Password is required'),
 })
 
-router.post('/login', (req, res) => {
-  res.status(201).json({ message: 'user login' })
-})
+// router.post('/register', (req, res) => {
+//   res.status(201).json({ message: 'user signed up' })
+// })
+
+router.post('/register', validateBody(insertUserSchema), register)
+
+// router.post('/login', (req, res) => {
+//   res.status(201).json({ message: 'user login' })
+// })
+
+router.post('/login', validateBody(loginSchema), login)
 
 export default router
